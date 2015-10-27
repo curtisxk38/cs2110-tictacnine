0b66c5526758640ec21a180208dfb30e1af86cf5
curtisxk38 <ck2ha@virginia.edu>
2015-10-27 19:24:52 -0400

    First commit

---
+++ src/TicTacNine.java
@@ -0,0 +1,16 @@
+
+public class TicTacNine {
+	int[][] smallBoard;
+	int[][] bigBoard;
+	
+	int x;
+	int o;
+	
+	public TicTacNine () {
+		smallBoard = new int[3][3];
+		bigBoard = new int[3][3];
+		
+		x = 1;
+		o = 1;
+	}
+}
